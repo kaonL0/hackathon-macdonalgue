79add402ad731520d46d7a2b20824f6857091209
Loic Druesne <loic.druesne@gmail.com>
2020-10-07 11:19:50 +0200

    Add build directory

---
+++ build/precache-manifest.af82cabb1d008b5ba3ef592958f6c420.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987244aab06aed1c893d71c4f2f349f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e56167893d372b1cafac",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6de895ddb82a437e6be5",
+    "url": "/static/js/2.2b3d4494.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b3d4494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e56167893d372b1cafac",
+    "url": "/static/js/main.1cd8090b.chunk.js"
+  },
+  {
+    "revision": "63735b08cbdbf5ac55cb",
+    "url": "/static/js/runtime-main.3deca924.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
